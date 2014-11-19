f26324d7b6a196db27fc971591bf4c75a9b6b8a8
sunyinhui <972883906@qq.com>
2014-11-19 22:28:52 +0800

    Update Utils.h
    delete

--- StaffManager/Utils.h
+++ StaffManager/Utils.h
@@ -22,7 +22,7 @@ public:
 	// Parameters controlling how often to keep new faces when collecting them. Otherwise, the training set could look to similar to each other!
 	double CHANGE_IN_IMAGE_FOR_COLLECTION;      // How much the facial image should change before collecting a new face photo for training.
 	double CHANGE_IN_SECONDS_FOR_COLLECTION ;       // How much time must pass before collecting a new face photo for training.
-
+  
 	void DrawPicToHDC(CDC *pDc, Mat &cameraFrame, UINT ID);
 	void GetPreprocessFaces(vector<Mat> &preprocessedFaces, Mat &displayedFrame, int faceWidth, CascadeClassifier &faceCascade, CascadeClassifier &eyeCascade1, CascadeClassifier &eyeCascade2, Rect &faceRect, Point &leftEye, Point &rightEye, Rect &searchedLeftEye, Rect &searchedRightEye,Mat &old_prepreprocessedFace, double &old_time);
 	bool CreatePath(CString m_strFolderPath);
